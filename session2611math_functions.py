a5c554fc6f3f64d478ba863c394f42e4c2194741
tanmmayrout <tanmmay.rout@diggibyte.com>
2024-11-26 14:20:05 +0100

    Python session 26th November math module

---
+++ session2611math_functions.py
@@ -0,0 +1,13 @@
+# Maths function practice November 26
+# Basic maths function common to other languages
+
+import  math
+
+#math to use math functions
+
+x=2.9
+print(round(x))
+
+y=-1123.333
+print(abs(y))
+
