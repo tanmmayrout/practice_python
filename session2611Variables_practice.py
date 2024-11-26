35a322ab68376a5c652dc29a0ef257e4c6a60784
tanmmayrout <tanmmay.rout@diggibyte.com>
2024-11-26 12:32:01 +0100

    Python session 26th November Variables Practice

---
+++ session2611Variables_practice.py
@@ -0,0 +1,13 @@
+#Variables practice 26 November
+# Variable types and initialisation
+
+price = 10 #int
+rating = 5.69 #float
+name= 'Tanmmay' #string
+
+is_published = False #Boolean True or False    #Python Case sensitive
+
+print(price)
+print(rating)
+print(name)
+print(is_published)
