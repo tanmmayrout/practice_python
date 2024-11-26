0a76742822a74e719fdee2e8e2904f5cea0ff712
tanmmayrout <tanmmay.rout@diggibyte.com>
2024-11-26 12:44:00 +0100

    Python session 26th November Inputs practice

---
+++ session2611Inputs_practice.py
@@ -0,0 +1,9 @@
+# Inputs practice 26th November
+
+name = input ('What is your name? ') #waits to take a message
+print('Hi ' + name) # Combines two strings to display message
+color = input ('What is your favourite color? ') #the space is for us to add values
+print (name + ' Likes ' + color) # concatonate by using space before and after
+
+
+# Basic input and out put # Very similar to MATLAB and C
