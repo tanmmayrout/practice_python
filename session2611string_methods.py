0cd8418eeacbcfec2dddc1cb25c25a12f71f286f
tanmmayrout <tanmmay.rout@diggibyte.com>
2024-11-26 13:38:56 +0100

    Python session 26th November string methods

---
+++ session2611string_methods.py
@@ -0,0 +1,15 @@
+#String methods practice November 26th
+
+name = 'My name is Tanmmay'
+print(name)
+print('Tanmmay in name')
+print('Tanmmay' in name)
+print('Chin2 in name')
+print('Chin2' in name)
+
+print('The length is',len(name))
+#String methods
+print('Converted  to upper',name.upper())
+print('Converted  to lower',name.lower())
+print(name.replace('Tanmmay','Chin2'))
+
