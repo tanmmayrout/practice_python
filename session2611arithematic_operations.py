29871f3538a6de5a210d081eac663932c8d8f01b
tanmmayrout <tanmmay.rout@diggibyte.com>
2024-11-26 14:14:28 +0100

    Python session 26th November arithmetic operations

---
+++ session2611arithematic_operations.py
@@ -0,0 +1,22 @@
+# Arithmatic operations practice 26th November
+#All similar to other programming languages
+print(10+3) #Normal math operations
+print(10/3) #Division
+print(10%3) #returns remainder
+
+#Operator precedence
+a = 10 ** 2
+print(a)
+
+
+x = 10
+x = x+3
+print(x)
+
+y = 3
+y += 3
+print(y)
+
+z = 10 + 3 * 2
+
+print(z)
