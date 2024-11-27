9c44985317b85cc3e9acf67ad3c7f66a27addf75
tanmmayrout <tanmmay.rout@diggibyte.com>
2024-11-27 13:48:20 +0100

    Python session 27th November If statement syntax and basics

---
+++ session2711is_statements.py
@@ -0,0 +1,14 @@
+#If statement practice 27th November
+
+#program just to test the syntax of if statements
+is_hot = True    #Boolean conditions
+is_cold = True
+
+if  is_hot:
+    print("Its a hot day") #executes under the indented  loop condition
+    print("Drink water")
+elif is_cold:
+    print("Its a cold day")
+    print("wear warm clothes")
+else:
+    print("Its a lovely day")
